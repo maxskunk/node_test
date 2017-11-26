64ad557609b83f1d47b4daf10ea5d407b22eb613
DESKTOP-QF1EDR5\brian <brian@brianweis.net>
2017-11-25 23:38:08 -0700

    Initial Upload!

---
+++ todoListApi/server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 6969,
+    mongoose = require('mongoose'),
+    Task = require('./models/todoListModel'), //created model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+var routes = require('./routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
