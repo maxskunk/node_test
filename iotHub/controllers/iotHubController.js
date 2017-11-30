464ee29747da8214d4f6554ad35456037ecdcba8
DESKTOP-QF1EDR5\brian <brian@brianweis.net>
2017-11-30 15:16:09 -0700

    Basic Connections!

--- iotHub/controllers/iotHubController.js
+++ iotHub/controllers/iotHubController.js
@@ -1,10 +1,19 @@
 'use strict';
+var activeDevices = [];
+// var mongoose = require('mongoose'),
+//     Device = mongoose.model('Device');
 
 
-exports.test_command = function (req, res) {
-   // Task.find({}, function (err, task) {
-        if (false)
-            res.send("FUCK: "+false);
-        res.json("Hello Gov");
-  //  });
+exports.connect_device = function (req, res) {
+
+    if (!req.body.deviceId) {
+        res.send("Connection Requires Valid Device Id");
+        console.log("Connection Denied for Invalid Device Id");
+    } else {
+        console.log('Connection From Device: ' + req.body.deviceId);
+        res.json({ "internalId": activeDevices.length });
+        activeDevices.push(req.body.typeId);
+        console.log("Active Devices: " + activeDevices.length);
+    }
+
 };
--- iotHub/routes/todoListRoutes.js
+++ iotHub/routes/todoListRoutes.js
@@ -1,19 +1,19 @@
 'use strict';
 module.exports = function (app) {
-    var todoList = require('../controllers/todoListController');
+    //var todoList = require('../controllers/todoListController');
     var iotHub = require('../controllers/iotHubController');
 
-    // todoList Routes
-    app.route('/tasks')
-        .get(todoList.list_all_tasks)
-        .post(todoList.create_a_task);
+    // // todoList Routes
+    // app.route('/tasks')
+    //     .get(todoList.list_all_tasks)
+    //     .post(todoList.create_a_task);
 
 
-    app.route('/tasks/:taskId')
-        .get(todoList.read_a_task)
-        .put(todoList.update_a_task)
-        .delete(todoList.delete_a_task);
+    // app.route('/tasks/:taskId')
+    //     .get(todoList.read_a_task)
+    //     .put(todoList.update_a_task)
+    //     .delete(todoList.delete_a_task);
 
-    app.route('/testCommand')
-        .get(iotHub.test_command)
+    app.route('/connect')
+        .post(iotHub.connect_device)
 };
