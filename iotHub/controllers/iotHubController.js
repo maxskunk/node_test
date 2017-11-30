61c66ec78e17f71410bf49786a06879accfed7d9
DESKTOP-QF1EDR5\brian <brian@brianweis.net>
2017-11-29 22:29:36 -0700

    iOtHub

---
+++ iotHub/controllers/iotHubController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+exports.test_command = function (req, res) {
+   // Task.find({}, function (err, task) {
+        if (false)
+            res.send("FUCK: "+false);
+        res.json("Hello Gov");
+  //  });
+};
